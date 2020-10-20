05edba4dcfb31785aefb5fdc40efddfc3885a892
leotalorac <leotalorac@unal.edu.co>
2020-10-20 12:47:29 -0500

    ready the function

--- main.go
+++ main.go
@@ -1,36 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func MinOf(vars ...int) int {
 	min := vars[0]
-
 	for _, i := range vars {
 		if min > i {
 			min = i
 		}
 	}
-
 	return min
-}
-func fillarray(arr []int, val int, n int) []int {
-	for i := 0; i < n; i++ {
-		arr = append(arr, val)
-	}
-	return arr
 }
 func getindexletter(l string, p int) int {
 	return int(l[p] - 97)
 }
 func distance(a string, b string) int {
-	var da [26]int
-	for i := 0; i < len(da); i++ {
-		da[i] = 0
-	}
+	// create the matrix
 	d := make([][]int, len(a)+2)
 	for i := range d {
 		d[i] = make([]int, len(b)+2)
 	}
+	// fill the matrix
 	maxdist := len(a) + len(b)
 	d[0][0] = maxdist
 	for i := 1; i < len(a)+2; i++ {
@@ -41,25 +33,35 @@ func distance(a string, b string) int {
 		d[0][j] = maxdist
 		d[1][j] = j - 1
 	}
+	// lastrow
+	lastrow := make(map[string]int)
 	fmt.Println(d)
 	for i := 1; i < len(a)+1; i++ {
-		db := 0
+		cha := string([]rune(a)[i-1])
+		lastmatchcol := 0
 		for j := 1; j < len(b)+1; j++ {
-			k := da[getindexletter(b, j)]
-			l := db
-			if getindexletter(a, i) == getindexletter(b, j) {
-				db = j
-			} else {
-				cost := 1
-				d[i+1][j+1] = MinOf(d[i][j]+cost,
-					d[i+1][j]+1,
-					d[i][j+1]+1,
-					d[k][l]+(i-k)+1+(j-l))
+			chb := string([]rune(b)[j-1])
+			var lastmatchingrow int = 0
+			if val, ok := lastrow[chb]; ok {
+				lastmatchingrow = val
+			}
+			cost := 1
+			if cha == chb {
+				cost = 0
+			}
+			d[i+1][j+1] = MinOf(d[i][j]+cost, // Substitution
+				d[i+1][j]+1, // Addition
+				d[i][j+1]+1, // Deletion
+				d[lastmatchingrow][lastmatchcol]+(i-lastmatchingrow-1)+1+(j-lastmatchcol-1))
+
+			if cost == 0 {
+				lastmatchcol = j
 			}
 		}
-		da[getindexletter(a, i)] = i
+		lastrow[cha] = i
 	}
 	fmt.Println(d)
+	fmt.Println(lastrow)
 	return d[len(a)][len(b)]
 }
 func main() {
